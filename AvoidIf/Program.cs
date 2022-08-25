4749e26aa458bb26e23c7eeec45708cc27e33b4e
Bayu <radamanthys_ok@yahoo.com>
2022-08-25 16:20:06 +0700

    commited

--- AvoidIf/Program.cs
+++ AvoidIf/Program.cs
@@ -4,3 +4,11 @@ var foundInt = a.FirstOrDefault(w => w == 4);
 
 Console.WriteLine($"{foundInt.ToString()}");
 Console.ReadKey();
+
+string monthName = "Juni";
+
+var tableMonth = new string[] { "Bulan", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember" };
+var listMonth = tableMonth.ToList();
+int month = listMonth.IndexOf(monthName);
+Console.WriteLine($"{month.ToString()}");
+Console.ReadKey();
